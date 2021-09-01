9477cb7ebf3b5dcee059943f47daf663d175efea
Himank Goel <himankgoel03@gmail.com>
2021-09-01 20:27:40 +0530

    added realtime detection using webcam

--- object_detection_tfod.py
+++ object_detection_tfod.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 from utils import label_map_util
+from utils import visualization_utils as vis_util
 import os
+import cv2
+import numpy as np
 
 MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
 PATH_TO_FROZEN_GRAPH = MODEL_NAME + '/frozen_inference_graph.pb'
@@ -15,3 +18,40 @@ with detection_graph.as_default():
         tf.import_graph_def(od_graph_def, name='')
 
 category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+cap = cv2.VideoCapture(0)
+
+with detection_graph.as_default():
+    config = tf.compat.v1.ConfigProto(log_device_placement=False)
+    config.gpu_options.allow_growth = False
+    with tf.compat.v1.Session(graph=detection_graph, config=config) as sess:
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+        while True:
+            ret, image_np = cap.read()
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+
+            (boxes, scores, classes, num) = sess.run(
+              [detection_boxes, detection_scores, detection_classes, num_detections],
+              feed_dict={image_tensor: image_np_expanded})
+
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=10)
+            
+            cv2.imshow('MobileNet SSD - Object Detection', image_np)
+            if cv2.waitKey(1) == 13: #13 is the Enter Key
+                break
+            
+# Release camera and close windows
+cap.release()
+cv2.destroyAllWindows()  
